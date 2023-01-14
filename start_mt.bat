f06bc9a0022d2c16b0736f4a4b1106b7dbcefc01
Sebastian M. Braun <sebastian.braun@fusca.de>
2023-01-14 22:55:56 +0100

    Update start_mt.bat
    correct sample path

--- start_mt.bat
+++ start_mt.bat
@@ -5,11 +5,11 @@ echo %date% - %time%
 
 :: Tester-Set: E:\Trading\MT5-2301\MQL5\Profiles\Tester\br504-016_US500.set
 
-E:\Trading\MT5-2301\terminal64.exe /portable /config:E:\Nextcloud\_sebra_ryzen\MT5-2301\tester_US500.ini
+E:\Trading\MT5-2301\terminal64.exe /portable /config:E:\Trading\MT5-2301\tester_US500.ini
 echo %date% - %time%
 
 :: Here you can put a lot of other setups ...
-::E:\Trading\MT5-2301\terminal64.exe /portable /config:E:\Nextcloud\_sebra_ryzen\MT5-2301\tester_US500.ini
+::E:\Trading\MT5-2301\terminal64.exe /portable /config:E:\Trading\MT5-2301\tester_US500.ini
 ::echo %date% - %time%
 
 
